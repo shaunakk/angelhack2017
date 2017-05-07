4305f8f136a69a2fd87f832da332b19a6b9ec671
Shaunak Kale <epicshaunak@gmail.com>
2017-05-06 18:03:47 -0700

    hi

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ app.use(express.static(path.join(__dirname, 'echoless', 'build')));
 
 app.get('*', (req, res) => {
       res.render(path.resolve(__dirname, 'echoless', 'build', 'index.html'));
-      app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/echoless/build/index.html');
-      });
+
 
       app.listen(app.get('port'), function() {
         console.log("Node app is running at localhost:" + app.get('port'));
