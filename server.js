4ee75c789df0c6c339a87ad7a87c030f0ec8a93d
Shaunak Kale <epicshaunak@gmail.com>
2017-05-06 18:05:24 -0700

    hi

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
+var express = require('express')
+var app = express()
+
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(path.join(__dirname, 'echoless', 'build')));
+app.use(express.static(__dirname + '/build'))
 
-app.get('*', (req, res) => {
-      res.render(path.resolve(__dirname, 'echoless', 'build', 'index.html'));
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-
-      app.listen(app.get('port'), function() {
-        console.log("Node app is running at localhost:" + app.get('port'));
-      })
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
