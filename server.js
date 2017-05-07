2039c2a2a537ac7a489b6c2dc94407abce321031
Conor Carey <ccarey@grove.co>
2017-05-06 17:30:52 -0700

    hopefully working server

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(path.resolve(__dirname, 'echoless', 'build')));
+app.use(express.static(path.resolve('./', 'echoless', 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'echoless', 'build', 'index.html'));
+app.get('/', (req, res) => {
+  res.render(path.resolve('./', 'echoless', 'build', 'index.html'));
 });
 
 app.listen(app.get('port'), function() {
