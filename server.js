60a0ef5febee19220217e4cf2214d425aad39655
Conor Carey <ccarey@grove.co>
2017-05-06 17:40:03 -0700

    hopefully working server

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(path.resolve('./', 'echoless', 'build')));
+app.use(express.static(path.join('./', 'echoless', 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('/', (req, res) => {
-  res.render(path.resolve('./', 'echoless', 'build', 'index'));
+  res.render(path.resolve('./', 'echoless', 'build', 'index.html'));
 });
 
 app.listen(app.get('port'), function() {
