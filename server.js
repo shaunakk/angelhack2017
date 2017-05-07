98396841d8c08e46bbf6ccde26d5af407fe7672f
Shaunak Kale <epicshaunak@gmail.com>
2017-05-06 18:07:46 -0700

    hi

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/build'))
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/build'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/build');
+app.set('view engine', 'jsx');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.render('pages/index');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log('Node app is running on port', app.get('port'));
+});
